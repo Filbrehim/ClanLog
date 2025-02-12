ce463e2e257c23f9d18a132fb448a97d6de0432c
Filbrehim <malignialourdsabot@gmail.com>
2025-02-12 07:08:56 +0100

    merge des scripts de mise à jour

--- bin/maj.sh
+++ bin/maj.sh
@@ -1,24 +1,41 @@
 #!/bin/bash
 
-DESTINATION=~/tmp365/data-clanlord
-## echo ${DESTINATION}
-mkdir -p ${DESTINATION}
+case $(hostname) in
 
-for destination in ${DESTINATION}
-do
+    dathomir )
+	
+	DESTINATION=~/tmp365/data-clanlord
+	mkdir -p ${DESTINATION}
+
+	for destination in ${DESTINATION}
+	do
+	    
+	    rsync --archive \
+		  ~/ClanLord/Text\ Logs/* ~cl.balangar/Copie/Text\ Logs/ \
+		  ${destination}
+
+	    systemctl --user start compile-clanlord.service &
+
+	    journalctl  --user --follow 
+
+	done
+	;;
+
+    falfaren )
+	
+	DESTINATION=data
+	for destination in ${DESTINATION} data
+	do
     
-rsync --archive \
-      ~/ClanLord/Text\ Logs/* ~cl.balangar/Copie/Text\ Logs/ \
-      ${destination}
+	    rsync --archive --verbose \
+		  ~/ClanLord.Shatrah.ro/ClanLord.win32/data/Text\ Logs/* ~/ClanLord/Text\ Logs/* \
+		  ${destination}
 
-done
+	done
+	;;
+esac
 
 cat <<EOF|tee {data,results,tmp}/CACHEDIR.TAG 
 Signature: 8a477f597d28d172789f06886806bc55
 dernières mise à jour $(date "+%A %e %B %T")
 EOF
-
-systemctl --user start compile-clanlord.service &
-
-journalctl  --user --follow 
-
